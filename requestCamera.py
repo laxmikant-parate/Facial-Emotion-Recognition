79f5a6f9eeb44c9950524c37a3a7ef396c340ae2
Laxmikant Parate <laxmikantparate01@gmail.com>
2021-12-07 21:01:03 +0530

    initial commit

---
+++ requestCamera.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+from model import FacialExpressionModel
+
+model = FacialExpressionModel()
+
+facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+all_emo = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Suprise"]
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        _, fr = self.video.read()
+        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]
+
+            roi = cv2.resize(fc, (48, 48))
+            text = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+            cv2.putText(fr, text, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+        _, jpeg = cv2.imencode('.jpg', fr)
+        return jpeg.tobytes()
